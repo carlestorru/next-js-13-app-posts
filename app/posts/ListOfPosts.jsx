eed5305e46515782439b8bd090e06b95fb663c48
carlestorru <carlestorru@gmail.com>
2022-11-02 18:39:38 +0100

    Initial commit

---
+++ app/posts/ListOfPosts.jsx
@@ -0,0 +1,34 @@
+import Link from 'next/link.js'
+import { LikeButton } from './LikeButton.jsx'
+
+const fetchPosts = async () => {
+  // getStaticProps
+  // ->  fetch('https://jsonplaceholder.typicode.com/posts')
+
+  // getServerSideProps
+  // ->  fetch('https://jsonplaceholder.typicode.com/posts', { cache: 'no-store' })
+
+  // incremental static regeneration
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts', {
+    next: {
+      revalidate: 60
+    }
+  })
+  const data = await res.json()
+
+  return data
+}
+
+export default async function ListOfPosts () {
+  const posts = await fetchPosts()
+
+  return posts.slice(0, 5).map(post => (
+    <article key={post.id}>
+      <Link href='/posts/[id]' as={`/posts/${post.id}`}>
+        <h2>{post.title}</h2>
+        <p>{post.body}</p>
+      </Link>
+      <LikeButton id={post.id} />
+    </article>
+  ))
+}
