9333770dc070e72657e76c0241740e74060797fa
carlestorru <carlestorru@gmail.com>
2022-11-05 10:55:31 +0100

    style for section comments

--- app/posts/[id]/comments/page.jsx
+++ app/posts/[id]/comments/page.jsx
@@ -1,4 +1,8 @@
 const fetchComments = async (id) => {
+  await new Promise(resolve => setTimeout(resolve, 1000))
+  throw new Error('Error al cargar los comentarios')
+
+  /*
   const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}/comments`, {
     next: {
       revalidate: 60
@@ -7,6 +11,7 @@ const fetchComments = async (id) => {
   const data = await res.json()
 
   return data
+  */
 }
 
 export default async function Post ({ params }) {
@@ -14,11 +19,11 @@ export default async function Post ({ params }) {
   const comments = await fetchComments(id)
 
   return (
-    <ul>
+    <ul style={{ background: '#999', fontSize: '12px' }}>
       {comments.map(comment => (
         <li key={comment.id}>
-          <h2>{comment.name}</h2>
-          <p>{comment.body}</p>
+          <h4>{comment.name}</h4>
+          <small>{comment.body}</small>
         </li>
       ))}
     </ul>
